78e9549f2d12e82a6237ac5f7c113bd17817ea49
Vincenzo <mattia.dipietro@aesystech.it>
2018-11-13 11:41:56 +0100

    finished

--- src/index.js
+++ src/index.js
@@ -1,10 +1,110 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { BrowserRouter, Route } from 'react-router-dom';
+import * as Redux from 'redux';
+import { Provider } from 'react-redux';
 import './index.css';
-import App from './App';
+import AuthorQuiz from './AuthorQuiz';
+import AddAuthorForm from './AddAuthorForm';
 import * as serviceWorker from './serviceWorker';
+import {shuffle, sample} from 'underscore';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+
+const authors = [
+    {
+        name: 'Mark Twan',
+        imageUrl: 'images/authors/MarkTwain.jpg',
+        imageSource: 'Wikimedia Commons',
+        books: ['The adventure of Huckleberry Finn'],
+    },
+    {
+        name: 'Joseph Conrad',
+        imageUrl: 'images/authors/JosephConrad.png',
+        imageSource: 'Wikimedia Commons',
+        books: ['Heart of Darkness'],
+    },
+    {
+        name: 'J.K. Rowling',
+        imageUrl: 'images/authors/JKrowling.jpg',
+        imageSource: 'Wikimedia Commons',
+        imageAttribution: 'Daniel Ogren',
+        books: ['Harry Potter and the Sorcerers Stone'],
+    },
+    {
+        name: 'Stephen King',
+        imageUrl: 'images/authors/StephenKing.jpg',
+        imageSource: 'Wikimedia Commons',
+        imageAttribution: 'Pinguino',
+        books: ['The Shining', 'IT'],
+    },
+    {
+        name: 'Charles Dickens',
+        imageUrl: 'images/authors/CharlesDickens.jpg',
+        imageSource: 'Wikimedia Commons',
+        books: ['David Copperfield', 'A Tale of Two Cities'],
+    },
+    {
+        name: 'William Shakespeare',
+        imageUrl: 'images/authors/WilliamShakespeare.jpg',
+        imageSource: 'Wikimedia Commons',
+        books: ['Hamlet', 'Macbeth', 'Romeo and Juliet'],
+    }
+];
+
+function getTurnData(authors) {
+    const allBooks = authors.reduce(function (p, c, i) {
+        return p.concat(c.books);
+    }, []);
+    
+    const fourRandomBooks = shuffle(allBooks).slice(0,4);
+    const answer = sample(fourRandomBooks);
+
+    return {
+        books: fourRandomBooks,
+        author: authors.find((author) => 
+            author.books.some((title) => 
+                title === answer))
+    }
+}
+
+function reducer(state = { authors, turnData: getTurnData(authors), highlight: '' }, action) {
+    switch (action.type) {
+        case 'ANSWER_SELECTED':
+            const isCorrect = state.turnData.author.books.some((book) => book === action.answer);
+            return Object.assign({}, state, { 
+                highlight: isCorrect ? 'correct' : 'wrong' 
+            });
+        case 'CONTINUE':
+            return Object.assign({}, state, {
+                highlight: '',
+                turnData: getTurnData(state.authors),
+            });
+        case 'ADD_AUTHOR':
+            return Object.assign({}, state, {
+                authors: state.authors.concat([action.author]),
+            });
+        default: 
+            return state;
+    }
+}
+
+let store = Redux.createStore(
+    reducer,
+    window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+    );
+
+ReactDOM.render(
+    <Provider store={store}>
+        <BrowserRouter>
+            <React.Fragment>
+                <Route exact path="/" component={AuthorQuiz}/>
+                <Route path="/Add" component={AddAuthorForm}/>
+            </React.Fragment>
+        </BrowserRouter>
+    </Provider>, 
+    document.getElementById('root')
+);
+
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
